efbfd9b8ac4f7f7efa22178e34b654ac4f7cd88d
Jean <jeanrinci.br1@gmail.com>
2023-12-12 19:09:06 -0500

    Secsion 1 del curso de Java

---
+++ src/com/company/Main.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class Main {
+    /**
+     * crear un proyecto desde cero en java con intelleji
+     * como hacer comentarios
+     * como imprimir desde con  System.out.println
+     * tipos de datos
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+
+        // types of dates
+
+        // Numeric
+        // 1. Enters
+        byte variable1 = 5;
+        short variable2 = 6;
+        int variable3 = 4;
+        long variable4 = 8;
+
+        //2. decimals
+        float variable6 = 5.5f;
+        double variable7 = 6.6d;
+
+        //3. boolean
+        boolean variable8 = false;
+        boolean variable9= true;
+
+        //4. texts
+        char variable10 = 'a';
+        String variable11 = "nice to meet you thansk jean";
+
+    }
+}
